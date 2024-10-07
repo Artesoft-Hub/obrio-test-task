672cb1ef047f9cefcbfc0a766a7e16d9dccb31c6
Kuzmenko Kyrylo <k.kuzmenko@artesoft.org>
2024-10-08 01:29:59 +0300

    add page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,26 @@
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { AnimatePresence } from "framer-motion";
 import { HistoryManagerProvider, useHistoryManager } from "@/ui/History";
 import RootLayout from "@/ui/RootLayout";
 import { store } from "@/data/store/store";
-import type { AppProps } from "next/app";
-import { Provider } from "react-redux";
 import Theme from "@/ui/theme";
+import FadeAnimation from "@/ui/atoms/FadeAnimation";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, router }: AppProps) {
     const historyManager = useHistoryManager();
 
     return (
         <HistoryManagerProvider value={historyManager}>
             <Theme>
                 <RootLayout>
-                    <Provider store={store}>
-                        <Component {...pageProps} />
-                    </Provider>
+                    <AnimatePresence mode="wait">
+                        <FadeAnimation key={router.asPath}>
+                            <Provider store={store}>
+                                <Component {...pageProps} />
+                            </Provider>
+                        </FadeAnimation>
+                    </AnimatePresence>
                 </RootLayout>
             </Theme>
         </HistoryManagerProvider>
