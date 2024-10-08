865581ff4b7b3031a42afd3ba55c8a0fde9b15b9
Artesoft-Hub <151553030+Artesoft-Hub@users.noreply.github.com>
2024-10-08 09:45:30 +0300

    Feat/improvements add animation and some major fixes (#3)
    * fix new answer selection and save response from text input
    
    * add page transition

--- data/store/reducers/index.ts
+++ data/store/reducers/index.ts
@@ -21,10 +21,25 @@ export const rootReducer = {
         action: PayloadAction<QuizID & QuestionID & Value>
     ) => {
         const { quizId, questionId, value } = action.payload;
-        state.results[quizId].answers.push({
-            questionId,
-            value,
-        });
+        const answers = state.results[quizId].answers;
+
+        // Find the index of the question if it was already answered
+        const existingAnswerIndex = answers.findIndex(
+            (answer) => answer.questionId === questionId
+        );
+
+        if (existingAnswerIndex !== -1) {
+            // If the question was already answered, replace it and remove subsequent answers
+            answers[existingAnswerIndex] = { questionId, value };
+            // Remove all answers after the current one
+            state.results[quizId].answers = answers.slice(
+                0,
+                existingAnswerIndex + 1
+            );
+        } else {
+            // If it's a new answer, just add it to the answers array
+            answers.push({ questionId, value });
+        }
     },
     setKey: (
         state: QuizzesState,
---
+++ ui/atoms/FadeAnimation.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { PropsWithChildren } from "react";
+
+const FadeAnimation = ({ children }: PropsWithChildren) => {
+    return (
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ ease: "easeInOut", duration: 0.3 }}
+        >
+            {children}
+        </motion.div>
+    );
+};
+
+export default FadeAnimation;
